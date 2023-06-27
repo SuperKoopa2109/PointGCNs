c20c5450c81da88904837d75409c19dccac8732f
SuperKoopa2109 <niklaskaspareit@gmail.com>
2023-06-27 14:06:04 +0200

    CHANGE: xavier initializer to new api call glorot_normal

--- utils/tf_util.py
+++ utils/tf_util.py
@@ -42,7 +42,7 @@ def _variable_with_weight_decay(name, shape, stddev, wd, use_xavier=True):
     Variable Tensor
   """
   if use_xavier:
-    initializer = tf.contrib.layers.xavier_initializer()
+    initializer = tf.keras.initializers.glorot_normal() #CHANGED: tf.contrib.layers.xavier_initializer()
   else:
     initializer = tf.truncated_normal_initializer(stddev=stddev)
   var = _variable_on_cpu(name, shape, initializer)
