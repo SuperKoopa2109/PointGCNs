f01648a21a7ca59c0ae63a2be0a7288b8cff3329
SuperKoopa2109 <niklaskaspareit@gmail.com>
2023-07-21 20:27:48 +0200

    CHANGE: TMP print statements for debug (2)

--- pointGCN/train.py
+++ pointGCN/train.py
@@ -298,8 +298,9 @@ def train_step(epoch, model, optimizer, loss, train_loader, device, config):
         optimizer.step()
         
         epoch_loss += l.item()
-        print(f'prediction {prediction}')
-        print(f'prediction.max(1) {prediction.max(1)}')
+        print(f'\n prediction {prediction} \n')
+        print(f'prediction.max(1) {prediction.max(1)} \n')
+        print(f"{prediction.max(1).shape}\n")
         correct += prediction.max(1)[1].eq(data['y']).sum().item()
     
     epoch_loss = epoch_loss / num_train_examples
