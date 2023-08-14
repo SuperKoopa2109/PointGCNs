5aece8cc8000ff48acdbc238df6796811a6701ac
SuperKoopa2109 <niklaskaspareit@gmail.com>
2023-08-14 22:00:02 +0200

    ADD: option for hyper param tuning using optuna

--- pointGCN/train.py
+++ pointGCN/train.py
@@ -2,6 +2,7 @@
 
 import argparse
 import numpy as np
+import random
 # import tensorflow as tf
 import socket
 import importlib
@@ -25,6 +26,7 @@ from tqdm import tqdm
 
 # For hyper parameter optimization
 import optuna
+from optuna.samplers import TPESampler
 
 # For logging
 import wandb
@@ -37,6 +39,8 @@ CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(CUR_DIR)
 
 sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, 'models'))
+sys.path.append(os.path.join(BASE_DIR, 'utils'))
 
 from param_config import param_config
 
@@ -105,58 +109,18 @@ class Custom_Parser():
     
     def parse_args(self):
         return self
+    
+class Config():
+            def __init__(self, kwconf = None, **kwargs):
+                if kwconf is not None:
+                    self.conf = {}
+                    for key, val in kwconf.items():
+                        setattr(self, key, val)
+                        
+            def __getitem__(self, key):
+                return getattr(self, key)
 
-# differentiate between running in an interactive shell vs shell
-RunningInCOLAB = False
-if is_running_in_jupyter():
-    print("*** Code is running in an interactive Shell. ***")
 
-    parser = Custom_Parser()
-
-elif is_running_in_colab():
-    print("*** Code is running in Google Colab. ***")
-
-    RunningInCOLAB = True
-    # BASE_DIR = os.path.join(BASE_DIR, 'pointGCNs')
-
-    parser = Custom_Parser(colab = True)
-
-else:
-    print("*** Code is running in a Shell. ***")
-
-    parser = argparse.ArgumentParser()
-    # parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU 0]')
-    # parser.add_argument('--model', default='pointnet_cls', help='Model name: pointnet_cls or pointnet_cls_basic [default: pointnet_cls]')
-    parser.add_argument('--log_dir', default='log', help='Log dir [default: log]')
-    # parser.add_argument('--num_point', type=int, default=1024, help='Point Number [256/512/1024/2048] [default: 1024]')
-    # parser.add_argument('--max_epoch', type=int, default=250, help='Epoch to run [default: 250]')
-    # parser.add_argument('--batch_size', type=int, default=32, help='Batch Size during training [default: 32]')
-    parser.add_argument('--learning_rate', type=float, default=0.001, help='Initial learning rate [default: 0.001]')
-    # parser.add_argument('--momentum', type=float, default=0.9, help='Initial learning rate [default: 0.9]')
-    # parser.add_argument('--optimizer', default='adam', help='adam or momentum [default: adam]')
-    # parser.add_argument('--decay_step', type=int, default=200000, help='Decay step for lr decay [default: 200000]')
-    # parser.add_argument('--decay_rate', type=float, default=0.7, help='Decay rate for lr decay [default: 0.8]')
-    parser.add_argument('--epochs', type=int, default=5, help='No of epochs for training [default: 5]')
-    parser.add_argument('--layers', type=int, default=3, help='No of hidden layers [default: 3]')
-    parser.add_argument('--dataset', default='shapenet', help='Dataset to be used for prediction [default: modelnet40]')
-    parser.add_argument('--colab', default='False', help='Code is executed in Google colab')
-    # FLAGS = parser.parse_args()
-
-FLAGS = parser.parse_args()
-
-# Check if code is executed in colab environment
-# if FLAGS.colab == 'True':
-    # BASE_DIR = os.path.join(BASE_DIR, 'PointGCNs')
-
-param_config.set_value('paths', 'BASE_DIR', BASE_DIR)
-param_config.set_value('paths', 'REPO_NAME', 'PointGCNs')
-param_config.set_value('system', 'dataset', FLAGS.dataset)
-param_config.set_value('system', 'RunningInCOLAB', FLAGS.colab)
-param_config.save()
-
-sys.path.append(BASE_DIR)
-sys.path.append(os.path.join(BASE_DIR, 'models'))
-sys.path.append(os.path.join(BASE_DIR, 'utils'))
 
 # **** END Import Section
 
@@ -170,23 +134,26 @@ sys.path.append(os.path.join(BASE_DIR, 'utils'))
 # DECAY_STEP = FLAGS.decay_step
 # DECAY_RATE = FLAGS.decay_rate
 
-LOG_DIR = FLAGS.log_dir
-if not os.path.exists(LOG_DIR): os.mkdir(LOG_DIR)
-# os.system('cp %s %s' % (MODEL_FILE, LOG_DIR)) # bkp of model def
-if FLAGS.colab == 'True': #is_running_in_colab():
-    os.system('cp PointGCNs/train.py %s' % (LOG_DIR)) # bkp of train procedure
-else:
-    os.system('cp train.py %s' % (LOG_DIR)) # bkp of train procedure
-LOG_FOUT = open(os.path.join(LOG_DIR, 'log_train.txt'), 'w')
-LOG_FOUT.write(str(FLAGS)+'\n')
 
-
-def log_string(out_str):
-    LOG_FOUT.write(out_str+'\n')
-    LOG_FOUT.flush()
+def log_string(out_str, log_file):
+    log_file.write(out_str+'\n')
+    log_file.flush()
     print(out_str)
 
-def load_data(config):
+def seed_everything(seed: int):
+    r"""Sets the seed for generating random numbers in :pytorch:`PyTorch`,
+    :obj:`numpy` and Python.
+
+    Args:
+        seed (int): The desired seed.
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    
+
+def load_data(config: object):
 
     # TODO: add edge features to graph -> Distances ? Degree ? Nearest Neighbourhood -> argsort of distnaces and one-hot-encoded?
     
@@ -252,10 +219,12 @@ def load_data(config):
     test_dataset = ShapeNet(
         root = config['savedir'] + "/" + config['model_name'] + "_test",
         categories = config['categories'],
-        transform=T.Compose([T.RadiusGraph(0.01),
-                                      T.Distance(),
-                                      T.OneHotDegree(50) 
-                                      ]), #T.OneHotDegree(50) just crashes if number too low  ## TODO: Check for highest degree on training data and use that
+        transform=T.Compose([
+                                    T.FixedPoints(2048,replace = False, allow_duplicates = True), 
+                                    T.RadiusGraph(0.01),
+                                    T.Distance(),
+                                    T.OneHotDegree(50) 
+                                ]), #T.OneHotDegree(50) just crashes if number too low  ## TODO: Check for highest degree on training data and use that
         split = "test"
     )
         
@@ -268,13 +237,44 @@ def load_data(config):
 
     return train_dataset, train_loader, val_dataset, val_loader, test_dataset, test_loader, vis_loader
 
-def load_model(modelname, input_dim, no_of_layers=3, hidden_dim=128, embed_dim=64, class_num=4):
+def load_model(
+        modelname, 
+        input_dim, 
+        embed_dim=64, 
+        hidden_dim=128, 
+        no_of_layers=3, 
+        class_num=4,
+        drop_rate = 0.,
+        negative_slope = 0., 
+        norm = 'None'
+        ):
+                
     if modelname == 'SageNet':
-        return SAGE_model(input_dim=input_dim, no_of_layers=no_of_layers, hidden_dim=hidden_dim, embed_dim=embed_dim, class_num=class_num)
+        return SAGE_model(
+             input_dim=input_dim, 
+             embed_dim=embed_dim, 
+             hidden_dim=hidden_dim, 
+             no_of_layers=no_of_layers, 
+             class_num=class_num,
+             drop_rate = drop_rate,
+             negative_slope=negative_slope,
+             norm=norm
+             )
     else:
-        return SAGE_model(input_dim=input_dim, no_of_layers=no_of_layers, hidden_dim=hidden_dim, embed_dim=embed_dim, class_num=class_num)
+        return SAGE_model(
+             input_dim=input_dim, 
+             embed_dim=embed_dim, 
+             hidden_dim=hidden_dim, 
+             no_of_layers=no_of_layers, 
+             class_num=class_num,
+             drop_rate = drop_rate,
+             negative_slope=negative_slope,
+             norm=norm
+             )
 
-def train():
+def objective(trial):
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     wandb_project = "pointGCNs"
     wandb_run_name = "playground/shapnet/1"
@@ -295,28 +295,9 @@ def train():
     else:
         print(f'{modulename} not imported || Run will only be logged locally')
 
-
-    
-
-        class Config():
-            def __init__(self, kwconf = None, **kwargs):
-                if kwconf is not None:
-                    self.conf = {}
-                    for key, val in kwconf.items():
-                        setattr(self, key, val)
-                        
-            def __getitem__(self, key):
-                return getattr(self, key)
-
         config = Config({
             "model_name": "ShapeNet",
             "categories": "Airplane",
-            "savedir": "data",
-            "batch_size": 32,
-            "num_workers": 1,
-            "epochs": 3,
-            "learning_rate": FLAGS.learning_rate,
-            "vis_sample_size": 5
         })
 
     config.seed = 42
@@ -324,47 +305,48 @@ def train():
     config.categories = "Airplane"
     config.savedir = "data"
     config.logdir = LOG_DIR
-    config.batch_size = 32
+    config.batch_size = trial.suggest_int('epoch_count', low=32, high=128, step=32)
     config.num_workers = 1
-    config.epochs = FLAGS.epochs
-    config.hidden_layers = FLAGS.layers
-    config.learning_rate = FLAGS.learning_rate
+    config.optimizer = trial.suggest_categorical("optimizer", ["MomentumSGD", "Adam"])
+    config.epochs = trial.suggest_int('epoch_count', low=50, high=200, step=50)
+    config.embed_dim=trial.suggest_int('embed_dim', low=64, high=128, step=64)
+    config.hidden_layers = trial.suggest_int("num_layers", 1, 4)
+    config.hidden_dim=trial.suggest_int('hidden_dim', low=128, high=256, step=128)
+    config.learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-2, log=True)
+    config.drop_rate = trial.suggest_float("dropout_rate", 0.0, 1.0)
+    config.negative_slope = trial.suggest_float("negative_slope", 0.0, 0.2)
+    config.norm_layer = trial.suggest_categorical("norm_layer", ["None", "Batch", "Instance"])
     config.vis_sample_size = 3
     config.wandb_run_name = wandb_run_name
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    
+    seed_everything(config.seed)
+
     table = wandb.Table(
-        columns=[
-            "Epoch",
-            "Ground-Truth",
-            "Predicted-Classes"
-        ]
-    )
+            columns=[
+                "Epoch",
+                "Ground-Truth",
+                "Predicted-Classes"
+            ]
+        )
 
     train_dataset, train_loader, val_dataset, val_loader, test_dataset, test_loader, vis_loader = load_data(config)
 
-
-    # Define PointNet++ model.
-
-    sample = next(iter(train_dataset)) #train_loader.dataset[0] #['y']
-
-    study = optuna.create_study()
+    sample = next(iter(train_dataset))
 
     model = load_model(
         modelname = 'SageNet', 
         input_dim = sample['x'].shape[1], 
+        embed_dim=trial.config.embed_dim, 
+        hidden_dim=config.hidden_dim,  #dimensions of first hidden layer
         no_of_layers = config.hidden_layers,
-        hidden_dim=128, 
-        embed_dim=64, 
-        class_num=int(sample['y'].max() + 1)).to(device)
-    print(model)
+        class_num=int(sample['y'].max() + 1),
+        drop_rate = config.drop_rate,
+        negative_slope = config.negative_slope, 
+        norm = config.norm_layer
+        ).to(device)
+                
 
-    # model = SAGE_model(
-         
-    #     128, 
-    #     256, 
-    #     class_num=int(sample['y'].max() + 1)).to(device)
+    print(model)
 
     # TODO: use .unique instead ?? 
 
@@ -374,19 +356,105 @@ def train():
         model.parameters(), lr=config.learning_rate
     )
 
+    optimizer = torch.optim.SGD(
+        model.parameters(), lr=config.learning_rate, momentum=0.9
+        )
+
     loss = nn.CrossEntropyLoss()
 
     for epoch in range(config.epochs):
         train_step(epoch, model, optimizer, loss, train_loader, device, config)
         val_step(epoch, model, loss, val_loader, device, config)
         visualize_evaluation(epoch, model, table, vis_loader, config, device)
-    # load model
 
-    
 
-    if is_imported('wandb'):
-        wandb.log({"PredClass_vs_TrueClass": table})
-        wandb.finish()
+def train(FLAGS, hyperparam_tuning = False):
+
+    if hyperparam_tuning == True:
+        study = optuna.create_study(sampler=TPESampler(), directions=["minimize"])
+        #study.optimize(objective, n_trials=100, timeout=300)
+    else:
+
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        wandb_project = "pointGCNs"
+        wandb_run_name = "playground/shapnet/1"
+
+        modulename = 'wandb'
+        if is_imported(modulename):
+            wandb.init(
+                project=wandb_project, 
+                #name=wandb_run_name, 
+                job_type="baseline-train"
+                )
+
+            wandb_run_name = wandb.run.name
+
+            # Set experiment configs to be synced with wandb
+            config = wandb.config
+
+        else:
+            print(f'{modulename} not imported || Run will only be logged locally')
+
+        config.seed = 42
+        config.model_name = "ShapeNet"
+        config.categories = "Airplane"
+        config.savedir = "data"
+        config.logdir = LOG_DIR
+        config.batch_size = 32
+        config.num_workers = 1
+        config.epochs = FLAGS.epochs
+        config.hidden_layers = FLAGS.layers
+        config.learning_rate = FLAGS.learning_rate
+        config.vis_sample_size = 3
+        config.wandb_run_name = wandb_run_name
+
+        seed_everything(config.seed)
+
+        table = wandb.Table(
+            columns=[
+                "Epoch",
+                "Ground-Truth",
+                "Predicted-Classes"
+            ]
+        )
+
+        train_dataset, train_loader, val_dataset, val_loader, test_dataset, test_loader, vis_loader = load_data(config)
+
+        sample = next(iter(train_dataset)) #train_loader.dataset[0] #['y']
+        
+        
+
+        model = load_model(
+            modelname = 'SageNet', 
+            input_dim = sample['x'].shape[1], 
+            no_of_layers = config.hidden_layers,
+            hidden_dim=128, 
+            embed_dim=64, 
+            class_num=int(sample['y'].max() + 1)).to(device)
+        print(model)
+
+        # TODO: use .unique instead ?? 
+
+
+        # Define Optimizer
+        optimizer = torch.optim.Adam(
+            model.parameters(), lr=config.learning_rate
+        )
+
+        loss = nn.CrossEntropyLoss()
+
+        for epoch in range(config.epochs):
+            train_step(epoch, model, optimizer, loss, train_loader, device, config)
+            val_step(epoch, model, loss, val_loader, device, config)
+            visualize_evaluation(epoch, model, table, vis_loader, config, device)
+        # load model
+
+        
+
+        if is_imported('wandb'):
+            wandb.log({"PredClass_vs_TrueClass": table})
+            wandb.finish()
 
 
 
@@ -442,7 +510,7 @@ def val_step(epoch, model, loss, val_loader, device, config):
     
     progress_bar = tqdm(
         range(num_val_examples),
-        desc=f"Validation Epoch {epoch}/{config.epochs}"
+        desc=f"Validation Epoch {epoch + 1}/{config.epochs}"
     )
     for batch_idx in progress_bar:
         data = next(iter(val_loader)).to(device)
@@ -564,5 +632,65 @@ def visualize_evaluation(epoch, model, table, vis_loader, config, device):
 
 if __name__ == "__main__":
 
-    train()
+
+    # differentiate between running in an interactive shell vs shell
+    RunningInCOLAB = False
+    if is_running_in_jupyter():
+        print("*** Code is running in an interactive Shell. ***")
+
+        parser = Custom_Parser()
+
+    elif is_running_in_colab():
+        print("*** Code is running in Google Colab. ***")
+
+        RunningInCOLAB = True
+        # BASE_DIR = os.path.join(BASE_DIR, 'pointGCNs')
+
+        parser = Custom_Parser(colab = True)
+
+    else:
+        print("*** Code is running in a Shell. ***")
+
+        parser = argparse.ArgumentParser()
+        # parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU 0]')
+        # parser.add_argument('--model', default='pointnet_cls', help='Model name: pointnet_cls or pointnet_cls_basic [default: pointnet_cls]')
+        parser.add_argument('--log_dir', default='log', help='Log dir [default: log]')
+        # parser.add_argument('--num_point', type=int, default=1024, help='Point Number [256/512/1024/2048] [default: 1024]')
+        # parser.add_argument('--max_epoch', type=int, default=250, help='Epoch to run [default: 250]')
+        # parser.add_argument('--batch_size', type=int, default=32, help='Batch Size during training [default: 32]')
+        parser.add_argument('--learning_rate', type=float, default=0.001, help='Initial learning rate [default: 0.001]')
+        # parser.add_argument('--momentum', type=float, default=0.9, help='Initial learning rate [default: 0.9]')
+        # parser.add_argument('--optimizer', default='adam', help='adam or momentum [default: adam]')
+        # parser.add_argument('--decay_step', type=int, default=200000, help='Decay step for lr decay [default: 200000]')
+        # parser.add_argument('--decay_rate', type=float, default=0.7, help='Decay rate for lr decay [default: 0.8]')
+        parser.add_argument('--epochs', type=int, default=5, help='No of epochs for training [default: 5]')
+        parser.add_argument('--layers', type=int, default=3, help='No of hidden layers [default: 3]')
+        parser.add_argument('--dataset', default='shapenet', help='Dataset to be used for prediction [default: modelnet40]')
+        parser.add_argument('--colab', default='False', help='Code is executed in Google colab [default: False]')
+        parser.add_argument('--train_hyperparams', default='False', help='Hyper param tuning [default: False]')
+        # FLAGS = parser.parse_args()
+
+    FLAGS = parser.parse_args()
+
+    # Check if code is executed in colab environment
+    # if FLAGS.colab == 'True':
+        # BASE_DIR = os.path.join(BASE_DIR, 'PointGCNs')
+
+    param_config.set_value('paths', 'BASE_DIR', BASE_DIR)
+    param_config.set_value('paths', 'REPO_NAME', 'PointGCNs')
+    param_config.set_value('system', 'dataset', FLAGS.dataset)
+    param_config.set_value('system', 'RunningInCOLAB', FLAGS.colab)
+    param_config.save()
+
+    LOG_DIR = FLAGS.log_dir
+    if not os.path.exists(LOG_DIR): os.mkdir(LOG_DIR)
+    # os.system('cp %s %s' % (MODEL_FILE, LOG_DIR)) # bkp of model def
+    if FLAGS.colab == 'True': #is_running_in_colab():
+        os.system('cp PointGCNs/train.py %s' % (LOG_DIR)) # bkp of train procedure
+    else:
+        os.system('cp train.py %s' % (LOG_DIR)) # bkp of train procedure
+    LOG_FOUT = open(os.path.join(LOG_DIR, 'log_train.txt'), 'w')
+    LOG_FOUT.write(str(FLAGS)+'\n')
+
+    train(FLAGS=FLAGS, hyperparam_tuning=True)
     LOG_FOUT.close()
